65a3b223efde3f7df47c8600953ab8ecdc811b3d
naveen kumar <naveen8296088066@gmail.com>
2025-10-28 11:56:16 +0530

    setupfile

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+
+    with open(file_path)as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if E_DOT in requirements:
+            requirements.remove(E_DOT)
+    return requirements
+
+
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'naveen',
+    author_email = 'naveen8296088066@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
